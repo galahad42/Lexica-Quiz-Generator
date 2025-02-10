5aee247c17f702842522fe7e683f36cb6bfde650
amrit <amritanand.dev@gmail.com>
2025-02-11 00:06:56 +0530

    fresh commit

---
+++ src/components/global/BugReport.tsx
@@ -0,0 +1,6 @@
+import Button from "@/components/shared/Button";
+import { BugIcon } from "../icons";
+
+export default function BugReport() {
+  
+}
---
+++ src/components/shared/GeminiBadge.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+import React from "react";
+import { GeminiIcon } from "../icons/Gemini";
+
+export default function GeminiBadge() {
+   
+}
